98faa475e43978ef49e3e2b7dc57ee52fc570f42
SiaESin <siabhon.sindab@gmail.com>
2023-06-05 00:16:38 -0700

    New restaurants can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,24 +1,25 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Coronado',
-        state: 'CA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/beachside-restaurant.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/cityview-restaurant.jpg'
-      }]
-      
-    res.render('places/index', {places})
+      res.render('places/index', {places})
 })
-router.post('/', (req, res) => {
-  res.send('POST /places stub')
+// router.post('/', (req, res) => {
+//   res.send('POST /places stub')
+// })
+router.post('/', (req, res) =>{
+   if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/450/450'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
 router.get('/new', (req, res) => {
   res.render('places/new')
---
+++ models/places.js
@@ -0,0 +1,15 @@
+
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Coronado',
+    state: 'CA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/beachside-restaurant.jpg'
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/cityview-restaurant.jpg'
+  }]
+  
