9f0c3cb3bc0dde83d3e96106512b8a4107a37ce5
SiaESin <siabhon.sindab@gmail.com>
2023-06-05 20:19:22 -0700

    connecting show route

--- views/home.jsx
+++ views/home.jsx
@@ -6,8 +6,8 @@ function home () {
         <Def>
             <main>
                 <h1>REST-Rant</h1>
-                <div>
-                    <img src="/images/pasta-dish.jpg" alt="A pasta and mushroom dish" height={600} width={850} />
+                <div className='homepage-image'>
+                    <img src="/images/pasta-dish.jpg" alt="A pasta and mushroom dish" height={700} width={1050} />
                     <div>
                     Photo by <a href="https://unsplash.com/@eaterscollective?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Eaters Collective</a> on <a href="https://unsplash.com/photos/ddZYOtZUnBk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
                     </div>
