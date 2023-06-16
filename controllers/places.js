c41faad915a4e7ee54b22274d5f7c543e890200c
SiaESin <siabhon.sindab@gmail.com>
2023-06-16 00:31:53 -0700

    adding comment form

--- controllers/places.js
+++ controllers/places.js
@@ -40,6 +40,28 @@ router.get('/:id', (req, res) => {
         res.render('error404')
     })
 })
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comments.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          console.log('err', err)
+          res.render('error404')
+      })
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
+})
 
 router.put('/:id', (req, res) => {
   res.send('PUT /places/:id stub')
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -11,12 +11,13 @@ function show (data) {
         comments = data.place.comments.map(c => {
           return (
             <div className="border">
-              <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
+              <h2 className="rant">{c.rant ? 'Rant! 🤬' : 'Rave! 💖'}</h2>
               <h4>{c.content}</h4>
               <h3>
                 <strong>- {c.author}</strong>
               </h3>
-              <h4>Rating: {c.stars}</h4>
+              <h4>Rating: {c.stars} ⭐️</h4> 
+              {/* <h2 className="recommend">{c.recommend ? 'Recommend! ✅' : 'Do not recommend 🚫'}</h2>  */}
             </div>
           )
         })
@@ -42,7 +43,7 @@ function show (data) {
                         <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
                             Edit
                             </a> 
-                            <br/>  
+                             
                             <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
                                 <button type="submit" className="btn btn-danger">
                                     Delete
@@ -51,7 +52,36 @@ function show (data) {
                         <h2>Comments</h2>
                         {comments}
                     </div>
-                </div>        
+                </div> 
+                <hr />
+            <h2>Does {data.place.name} deserve a Rave 💖 or a Rant 🤬?</h2>
+            <form action={`/places/${data.place.id}/comment`} method="POST">
+              <div className="row">
+                <div className="form-group col-sm-12">
+                  <label htmlFor="content">Content</label>
+                  <textarea id="content" name="content" className="form-control"></textarea>
+                </div>
+              </div>
+              <div className="row">
+                <div className="form-group col-sm-4">
+                  <label htmlFor="author">Author</label>
+                  <input id="author" name="author" className="form-control" />
+                </div>
+                <div className="form-group col-sm-4">
+                  <label htmlFor="stars">⭐️ Star Rating</label>
+                  <input type="range" step="0.5" min="1" max="5" id="stars" name="stars" className="form-control" />
+                </div>
+                <div className="form-group col-sm-2">
+                  <label htmlFor="rant">Rant?</label>
+                  <input type="checkbox" id="rant" name="rant" className="form-control" />
+                </div>
+                {/* <div className="form-group col-sm-2">
+                  <label htmlFor="recommend">Recommend?</label>
+                  <input type="checkbox" id="recommend" name="recommend" className="form-control" />
+                </div> */}
+              </div>
+              <input type="submit" className="btn btn-primary" value="Add Comment" />
+            </form>       
             </main>
         </Def>
     )
